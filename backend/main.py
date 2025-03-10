f117e2dd5b61d84f287c6cb940c635a30e390e3a
fjlongland <fjlongland@gmail.com>
2025-03-10 16:28:59 +0200

    can now connect to db, its not here because i need to setup .env first

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 
+from database import dbModels as models
+from database.database import engine
+ 
+
+models.Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
