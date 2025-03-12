b502cfa7cdaa0d8361dfd7ace312b84e42cac47b
fjlongland <fjlongland@gmail.com>
2025-03-12 13:27:56 +0200

    you can now create and delete a user

--- backend/routers/user_router.py
+++ backend/routers/user_router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from database.database import get_db
+from database import dbModels
 
 
 router = APIRouter(prefix="/users", 
@@ -10,3 +11,29 @@ router = APIRouter(prefix="/users",
 @router.get("/test")
 def test_user_rout(db: Session = Depends(get_db)):
     return{"Users": "Works"}
+
+@router.post("/")
+def new_user(db: Session = Depends(get_db)):
+
+    new_user = dbModels.User(user_name = 'test username',
+                             user_password = 'test passeord',)
+    
+    db.add(new_user)
+    db.commit()
+
+    return{"test user": "crested"}
+
+@router.delete("/{id}")
+def delete_user(id: int, 
+                db: Session = Depends(get_db)):
+
+    delete_user = db.query(dbModels.User).filter(dbModels.User.user_id == id).first()
+
+    if delete_user == None:
+        print("no users in table")
+
+    db.delete(delete_user)
+    db.commit()
+
+    return {f"User {id}": "Deleted"}
+
